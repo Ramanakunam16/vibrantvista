f02310b128575266ba49368444ae9c3556b35993
ramanakunam16 <ramanakunam16@gmail.com>
2024-02-23 19:42:37 +0530

    majaor commit

--- src/Heading.js
+++ src/Heading.js
@@ -1,11 +1,13 @@
 import { Slides } from "./Slides";
 import { Drop } from "./Drop";
+import logo from "./logo.PNG";
 
 export function Heading() {
   return (
     <>
       <Drop />
       <Slides />
+      <img src={logo} alt={"logo"} className="logopic" />
       <div className="logo">
         <h2>VibrantVista`24 </h2>
         <h2>VibrantVista`24 </h2>
